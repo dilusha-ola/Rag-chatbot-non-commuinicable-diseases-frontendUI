8650a93899999481b279000f9298418bdba65df5
dilusha-ola <dilushaolaboduwa3@gmail.com>
2025-12-18 17:06:40 +0530

    create three main body sections in UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,26 @@ function App() {
   return (
     <>
       <div className="min-h-screen  bg-white">
+        {/* Header */}
         <div className="text-3xl text-white font-bold bg-blue-500 px-8 py-4">NCD Health Assistant</div>
-        <div>
+        {/* Body */}
+          <div className="flex w-full ">
 
-        </div>
-        <div>
+              {/*chat History*/}
+               <div className="w-1/5">
+                  <div className="text-xl font-semibold px-8 py-4">Chat History</div>
+               </div>
 
-        </div>
+              {/* Input Box */}
+              <div>
+                <div className="text-xl font-semibold px-8 py-4">Input Box</div>
+              </div>
+
+              {/*References section*/}
+              <div className="w-1/5">
+                <div className="text-xl font-semibold px-8 py-4">References Section</div>
+              </div>
+          </div>
       </div>
         
     </>
